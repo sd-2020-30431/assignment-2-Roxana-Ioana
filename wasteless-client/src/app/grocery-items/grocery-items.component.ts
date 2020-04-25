eb304bf1c122a129b72565a5577081a9660f6071
Roxi <aldearoxanaioana@gmail.com>
2020-04-25 16:39:38 +0300

    client side added, login and grocery lists

---
+++ wasteless-client/src/app/grocery-items/grocery-items.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grocery-items',
+  templateUrl: './grocery-items.component.html',
+  styleUrls: ['./grocery-items.component.css']
+})
+export class GroceryItemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ wasteless-client/src/app/login/login.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import {LoginService} from '../login.service'
+import { Router, ActivatedRoute } from '@angular/router';
+import { User } from './user';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  
+  data1:User;
+  user: User=new User();
+  submitted = false;
+
+  constructor(private loginService: LoginService,
+    private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+  }
+
+  findUser()
+  {
+     this.loginService.findUser(this.user)
+      .subscribe(data=>{console.log(data),
+                        this.goToNextPage(data);
+                        },
+                 error=>{console.log("Fail to log in")});
+
+    this.user = new User();
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.findUser();    
+  }
+
+  goToNextPage(idUser:Object) {
+    this.router.navigate(['/grocery-lists', idUser]);
+  }
+
+}
